8351bc5dd9858b7d76ccdf8b43f01091a32e2e49
Seung <seung0708@github.com>
2020-10-11 08:41:20 -0700

    updated products controller

--- backend/ecommerce-project-api/app/controllers/api/v1/products_controller.rb
+++ backend/ecommerce-project-api/app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+    
 
     def index
         products = Product.all
@@ -9,5 +10,41 @@ class Api::V1::ProductsController < ApplicationController
         product = Product.find(params[:id])
         render json: product
     end
+
+    def current_user_products
+        if logged_in?
+            @products = current_user.products 
+            render json: products
+        else
+            render json: { notice: "You haven't uploaded any products!"}
+        end 
+    end 
+
+    def create
+        @product = current_user.products.build(product_params)
+        if @product.save
+            render json: products, status: created
+        else 
+            error_resp = {error: @product.errors.full_messages.to_sentence}
+            render json: error_resp, status: unprocessable_entity
+        end 
+    end
+
+    def update
+        @product = Product.find_by(id: params[:id])
+        if @product.update(product_params)
+            render json: product
+        else 
+            error_resp = {
+                error: @product.errors.full_messages.to_sentence,
+                invalid_user: "You don't have acces to this product"
+            }
+            render json: error_resp, status: unprocessable_entity
+        end 
+    end
+
+    def product_params
+        params.require(:product).permit(:title, :description, :image, :price)
+    end 
 
 end
